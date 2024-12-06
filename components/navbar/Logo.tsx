c3699b5bf2a693309383cde5709ca40c6dae860a
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-12-06 12:48:25 -0800

    Add react-icons and initial Logo and Navsearch

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import Link from 'next/link';
+import { LuTent } from 'react-icons/lu';
+import { Button } from '../ui/button';
 
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <LuTent className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import { Input } from '../ui/input';
 
 export default function NavSearch() {
-  return <div>NavSearch</div>;
+  return (
+    <Input
+      type="text"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted"
+    />
+  );
 }
