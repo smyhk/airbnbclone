2277a6f61f6f88ff14a5f42f1b2a43815e2ec6b5
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-12-06 11:49:29 -0800

    Setup component folders and get UI components from shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import { Button } from '@/components/ui/button';
 
 export default function HomePage() {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me!
+      </Button>
+    </div>
+  );
 }
