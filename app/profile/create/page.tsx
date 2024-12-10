56746913b729efba76e1e165f0646cfd8b46d70f
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-12-10 10:34:01 -0800

    Implement reusable FormInput component

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/button';
+
+const createProfileAction = async (formData: FormData) => {
+  'use server';
+
+  const firstName = formData.get('firstName') as string;
+  console.log(firstName);
+};
+
+export default function CreateProfilePage() {
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
+      <div className="border p-8 rounded-md max-w-lg">
+        <form action={createProfileAction}>
+          <Button type="submit" size="lg">
+            Create Profile
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
+}
