fb506a05005f8206b363dcd96eb48ee85bd97028
Steve Kedzie <seal.team.smyhk@gmail.com>
2026-01-01 15:51:19 -0800

    Enable theme switching

---
+++ app/providers.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+import { ThemeProvider } from './theme-provider';
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
+}
