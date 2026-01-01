963695ea7152aaf07b7f29be9c4e244634cfd924
Steve Kedzie <seal.team.smyhk@gmail.com>
2025-12-31 21:10:51 -0800

    Implement logo and search input

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import NavSearch from './NavSearch';
+import LinksDropdown from './LinksDropdown';
+import DarkMode from './DarkMode';
+import Logo from './Logo';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="border-b">
+      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </div>
+    </nav>
+  );
 }
