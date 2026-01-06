5d4cb9826898eb627b580aa5d4c5050888c353d3
Steve Kedzie <seal.team.smyhk@gmail.com>
2026-01-06 15:22:07 -0800

    Define reusable image input

---
+++ components/form/ImageInput.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+
+export default function ImageInput() {
+  const name = 'image';
+
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        Image
+      </Label>
+      <Input
+        id={name}
+        name={name}
+        type="file"
+        required
+        accept="image/*"
+        className="max-w-xs"
+      />
+    </div>
+  );
+}
