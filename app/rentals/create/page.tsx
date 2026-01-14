4858a45b4ba51365a9239929ad9f3a10f302cea9
Steve Kedzie <seal.team.smyhk@gmail.com>
2026-01-13 18:15:49 -0800

    Implement text area input and render on rental create page

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -4,6 +4,7 @@ import { createPropertyAction } from '@/utils/actions';
 import { SubmitButton } from '@/components/form/Buttons';
 import PriceInput from '@/components/form/PriceInput';
 import CategoriesInput from '@/components/form/CategoriesInput';
+import TextAreaInput from '@/components/form/TextAreaInput';
 
 export default function CreatePropertyPage() {
   return (
@@ -33,6 +34,10 @@ export default function CreatePropertyPage() {
             <CategoriesInput />
           </div>
           {/* text area / description */}
+          <TextAreaInput
+            name="description"
+            labelText="description (10 - 1000 words)"
+          />
           <SubmitButton text="create rental" className="mt-12" />
         </FormContainer>
       </div>
