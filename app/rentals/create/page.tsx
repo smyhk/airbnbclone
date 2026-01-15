7cf88734d09eab100e1098b061f04a26cf4132b1
Steve Kedzie <seal.team.smyhk@gmail.com>
2026-01-14 17:00:12 -0800

    Implement CounterInput and render on create rental page

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -7,6 +7,7 @@ import CategoriesInput from '@/components/form/CategoriesInput';
 import TextAreaInput from '@/components/form/TextAreaInput';
 import CountriesInput from '@/components/form/CountriesInput';
 import ImageInput from '@/components/form/ImageInput';
+import CounterInput from '@/components/form/CounterInput';
 
 export default function CreatePropertyPage() {
   return (
@@ -44,6 +45,13 @@ export default function CreatePropertyPage() {
             <CountriesInput />
             <ImageInput />
           </div>
+          <h3 className="text-lg mt-8 mb-4 font-medium">
+            Accomodation Details
+          </h3>
+          <CounterInput detail="guests" />
+          <CounterInput detail="bedroooma" />
+          <CounterInput detail="beds" />
+          <CounterInput detail="baths" />
           <SubmitButton text="create rental" className="mt-12" />
         </FormContainer>
       </div>
